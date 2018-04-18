bbdbe86494b8de36e5e797866083083c93a35fc9
sharath kumar <yukisharu@YukShas-MacBook-Pro.local>
2018-04-17 21:24:04 -0500

    gcloud init

---
+++ src/main/java/com/sharath/gcloud/app/gcloudbootapp/GcloudBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.sharath.gcloud.app.gcloudbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcloudBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcloudBootAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sharath/gcloud/app/gcloudbootapp/GcloudBootAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sharath.gcloud.app.gcloudbootapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GcloudBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
